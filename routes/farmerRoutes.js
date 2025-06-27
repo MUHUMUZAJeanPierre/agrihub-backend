a0f4e7a50eb5051dc317eb57829e2d7254cf9925
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2025-06-27 10:32:02 +0200

    feat

--- routes/farmerRoutes.js
+++ routes/farmerRoutes.js
@@ -6,6 +6,7 @@ router.get('/farmers', farmerController.getAllFarmers);
 router.get('/farmers/:id', farmerController.getFarmerById);
 router.post('/farmers', farmerController.createFarmer);
 router.put('/farmers/:id', farmerController.updateFarmer);
+
 router.delete('/farmers/:id', farmerController.deleteFarmer);
 
 module.exports = router;
