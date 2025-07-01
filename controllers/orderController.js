2f0ec1d350a549beafa63196ad66a9d196b6aed8
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2025-07-01 09:38:24 +0200

    feat

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -72,7 +72,7 @@ exports.placeOrder = async (req, res) => {
     const order = new Order({
       user: req.user.id,
       items: cart.items.map(item => ({
-        product: item.product._id,
+        product: item._id,
         quantity: item.quantity,
       })),
       totalAmount,
