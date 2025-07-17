b4e254408dabeb2215b2dbfc2b950e7c8f200f9e
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2025-07-17 14:45:38 +0200

    feat

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 const authMiddleware = require('../middleware/authMiddleware');
-router.get('/', authMiddleware,productController.getAllProducts);
-router.get('/:id',authMiddleware, productController.getProductById);
-router.post('/', authMiddleware, productController.createProduct);
-router.put('/:id', authMiddleware, productController.updateProduct);
-router.delete('/:id', authMiddleware,productController.deleteProduct);
+router.get('/', productController.getAllProducts);
+router.get('/:id', productController.getProductById);
+router.post('/',  productController.createProduct);
+router.put('/:id',  productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
 
 
 module.exports = router;
